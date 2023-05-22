b3dd71211af0fdfe9e8bfaa5a13bf0ff1249efd8
DhinakaranS-75 <dhinakaran75493@gmail.com>
2023-05-22 23:08:54 +0530

    my code

---
+++ src/Card/Card.js
@@ -0,0 +1,167 @@
+import React from 'react'
+import './Card.css'
+
+function Card() {
+  return (
+    <div>
+      <div className='Shoping' id='1'>
+        <div className='Shoping__item'>
+          <div className='Shoping__item__image'>
+            <img src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="" />
+          </div>
+          <hr />
+          <div className='Shoping__item__text'>
+            <h2>Fancy Product</h2>
+            <h5>$40.00 - $80.00</h5>
+          </div>
+        </div>
+        <div className="Card_Footer">
+          <a href="#" >View Options</a>
+        </div>
+      </div>
+
+      <div className='Shoping' id='2'>
+        <div className='sales-icon'>Sale</div>
+        <div className='Shoping__item'>
+          <div className='Shoping__item__image'>
+            <img src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="" />
+          </div>
+          <hr />
+          <div className='Shoping__item__text'>
+            <h2>Fancy Product</h2>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <h5><span>$40.00</span>
+            $80.00</h5>
+          </div>
+        </div>
+        <div className="Card_Footer">
+          <a href="#" >View Options</a>
+        </div>
+      </div>
+
+      <div className='Shoping' id='3'>
+        <div className='Shoping__item'>
+          <div className='Shoping__item__image'>
+            <img src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="" />
+          </div>
+          <hr />
+          <div className='Shoping__item__text'>
+            <h2>Fancy Product</h2>
+            <h5><span>$40.00</span>
+            $80.00</h5>
+          </div>
+        </div>
+        <div className="Card_Footer">
+          <a href="#" >View Options</a>
+        </div>
+      </div>
+
+      <div className='Shoping' id='4'>
+        <div className='Shoping__item'>
+          <div className='Shoping__item__image'>
+            <img src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="" />
+          </div>
+          <hr />
+          <div className='Shoping__item__text'>
+            <h2>Fancy Product</h2>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <h5>$40.00 - $80.00</h5>
+          </div>
+        </div>
+        <div className="Card_Footer">
+          <a href="#" >View Options</a>
+        </div>
+      </div>
+
+
+      <div className='Shoping' id='5'>
+        <div className='sales-icon'>Sale</div>
+        <div className='Shoping__item'>
+          <div className='Shoping__item__image'>
+            <img src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="" />
+          </div>
+          <hr />
+          <div className='Shoping__item__text'>
+            <h2>Fancy Product</h2>
+            <h5><span>$40.00</span>
+            $80.00</h5>
+          </div>
+        </div>
+        <div className="Card_Footer">
+          <a href="#" >View Options</a>
+        </div>
+      </div>
+
+      <div className='Shoping' id='6'>
+        <div className='Shoping__item'>
+          <div className='Shoping__item__image'>
+            <img src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="" />
+          </div>
+          <hr />
+          <div className='Shoping__item__text'>
+            <h2>Fancy Product</h2>
+            <h5>$40.00 - $80.00</h5>
+          </div>
+        </div>
+        <div className="Card_Footer">
+          <a href="#" >View Options</a>
+        </div>
+      </div>
+
+      <div className='Shoping' id='7'>
+        <div className='sales-icon'>Sale</div>
+        <div className='Shoping__item'>
+          <div className='Shoping__item__image'>
+            <img src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="" />
+          </div>
+          <hr />
+          <div className='Shoping__item__text'>
+            <h2>Fancy Product</h2>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <h5><span>$40.00</span>
+            $80.00</h5>
+          </div>
+        </div>
+        <div className="Card_Footer">
+          <a href="#" >View Options</a>
+        </div>
+      </div>
+
+      <div className='Shoping' id='8'>
+        <div className='Shoping__item'>
+          <div className='Shoping__item__image'>
+            <img src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="" />
+          </div>
+          <hr />
+          <div className='Shoping__item__text'>
+            <h2>Fancy Product</h2>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <i class="fa fa-star" aria-hidden="true"></i>
+            <h5>$40.00 - $80.00</h5>
+          </div>
+        </div>
+        <div className="Card_Footer">
+          <a href="#" >View Options</a>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Card;
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div>
+      <div class="navbar">
+        <h3 className='logo'>Start Bootstarp</h3>
+        <a href="#home">Home</a>
+        <a href="#news">About</a>
+        <div class="dropdown">
+          <button class="dropbtn">Shop 
+          <i class="fa fa-caret-down"></i>
+          </button>
+          <div class="dropdown-content">
+          <a href="#">All Products</a>
+          <hr />
+          <a href="#">Popular Items</a>
+          <a href="#">New Arrivals</a>
+          </div>
+        </div> 
+        <button className="btn" type="submit">
+          <i className="fa fa-shopping-cart" aria-hidden="true"></i>
+          Cart
+          <span >0</span>
+        </button>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
